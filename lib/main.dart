620b89a5a0f72635d6be3eeb2cacea99caa05395
DESKTOP-NSPHEG6\Admin <axel26alex@yahoo.com>
2025-04-16 17:31:50 +0300

    Main_Functionality_Update_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,71 @@
+/// The purpose of this project is to create a news app,
+/// with a user-friendly interface and an efficient implementation.
+///
+/// Made by: Andrei Voicea
+/// Project Location: https://github.com/AndreiVoicea2
+///
+
 import 'package:flutter/material.dart';
+import 'screens/news_list_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp();
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool darkMode = false;
+
+
+  final ThemeData lightTheme = ThemeData(
+    useMaterial3: false,
+    primaryColor: const Color(0xFFFFF8E1),
+    scaffoldBackgroundColor: const Color(0xFFFFF8E1),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFFFFE0B2),
+      foregroundColor: Colors.black,
+    ),
+    cardColor: const Color(0xFFFFF3E0),
+  );
+
+
+  final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    useMaterial3: false,
+    primaryColor: Colors.grey[900],
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.grey[850],
+      foregroundColor: Colors.white,
+    ),
+    cardColor: Colors.grey[800],
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'News App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('News'),
-      ),
-      body: const Center(
-        child: Text(
-          'Hello World2!',
-          style: TextStyle(fontSize: 24),
+      theme: darkMode ? darkTheme : lightTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("News"),
         ),
+        body: NewsListPage(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              darkMode = !darkMode;
+            });
+          },
+          child: const Icon(Icons.brightness_6),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       ),
     );
   }
---
+++ lib/models/news_item.dart
@@ -0,0 +1,15 @@
+class NewsItem {
+  final String title;
+  final String publicationDate;
+  final String author;
+  final int numComments;
+  final int points;
+
+  NewsItem({
+    required this.title,
+    required this.publicationDate,
+    required this.author,
+    required this.numComments,
+    required this.points,
+  });
+}
