fc517e421ee88b9ee5b34861cb91a293e58c8003
DESKTOP-NSPHEG6\Admin <axel26alex@yahoo.com>
2025-04-20 11:35:20 +0300

    Main_Functionality_Update_5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     ndkVersion = "27.0.12077973"
